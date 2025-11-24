659c1989df4432ac7e7f5251823edbb702a2943f
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 17:11:56 -0500

    made a login form and made the username and password controlled variables

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 function Login(){
     const LoginAPI = "https://6924d26482b59600d7217be2.mockapi.io/LoginVerification"
     const navigate = useNavigate()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     
-    function handleLogin(){
+    function handleLogin(e){
+        e.preventDefault()
         fetch(LoginAPI)
         .then(response=>response.json())
         .then((data)=>{
@@ -15,7 +19,13 @@ function Login(){
     return(
         <>
             <h2>Login Page</h2>
-            <button onClick={handleLogin}>Login</button>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="username">Username: </label>
+                <input type="text" placeholder="username" id="username" value={username} onChange={(e)=>setUsername(e.target.value)} autoComplete="username"></input>
+                <label htmlFor="password">Password: </label>
+                <input type="text" placeholder="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+                <button type="submit">Login</button>
+            </form>
         </>
     )
 }
