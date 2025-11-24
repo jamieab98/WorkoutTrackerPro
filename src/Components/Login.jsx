a9958cd613fcabe4e2829a24d0575a27753984f6
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 17:02:39 -0500

    Created the fetch command when the login button is clicked

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,15 @@
 import { useNavigate } from "react-router-dom"
 
 function Login(){
+    const LoginAPI = "https://6924d26482b59600d7217be2.mockapi.io/LoginVerification"
     const navigate = useNavigate()
+    
     function handleLogin(){
-        navigate("/home")
+        fetch(LoginAPI)
+        .then(response=>response.json())
+        .then((data)=>{
+            console.log(data)
+        })
     }
 
     return(
