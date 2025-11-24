b99448e62ed80741688b7fc5b4f4e0aa30cfc398
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 18:36:01 -0500

    Home page successfully fetches the data from the user that logged in

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,16 @@
 import NavigationBar from "./NavigationBar"
 import { UsernameContext } from "../UsernameContext"
 import { useContext, useEffect } from "react"
+
 function Home(){
     const {username} = useContext(UsernameContext)
     const workoutsAPI = `https://6924d26482b59600d7217be2.mockapi.io/${username}workouts`
     useEffect(()=>{
         fetch(workoutsAPI)
         .then(response=>response.json())
-        .then(data=>console.log(data))
+        .then((data)=>{
+            console.log(data)
+        })
     })
 
     return(
