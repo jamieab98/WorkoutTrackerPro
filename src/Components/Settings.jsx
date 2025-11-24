a7ad5b6f09ace0e14a8650fa988708ae53f29ab4
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 16:44:27 -0500

    Created a logout link on the settings page

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,10 +1,12 @@
 import NavigationBar from "./NavigationBar"
+import { Link } from "react-router-dom"
 function Settings(){
 
     return(
         <>
             <NavigationBar/>
             <h2>Settings Page</h2>
+            <Link to="/">Log Out</Link>
         </>
     )
 }
