fb3cfd06ca08930e440ec49bbaa18c37dc11a508
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 18:42:51 -0500

    Made the dailyworkout component and passed the day prop there for every object

---
+++ src/Components/DailyWorkout.jsx
@@ -0,0 +1,10 @@
+function DailyWorkout({day}){
+
+    return(
+        <>
+            <h3>Daily Workout</h3>
+            <div>{day.date}</div>
+        </>
+    )
+}
+export default DailyWorkout
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,28 @@
 import NavigationBar from "./NavigationBar"
 import { UsernameContext } from "../UsernameContext"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
+import DailyWorkout from "./DailyWorkout"
 
 function Home(){
     const {username} = useContext(UsernameContext)
+    const [workoutData, setWorkoutData] = useState([])
     const workoutsAPI = `https://6924d26482b59600d7217be2.mockapi.io/${username}workouts`
     useEffect(()=>{
         fetch(workoutsAPI)
         .then(response=>response.json())
         .then((data)=>{
-            console.log(data)
+            setWorkoutData(data)
         })
-    })
+    }, [])
 
     return(
         <>
             <NavigationBar/>
             <h2>Home Page</h2>
             <p>Welcome {username}!</p>
+            {workoutData.map((day)=>(
+                <DailyWorkout day={day} key={day.id}/>
+            ))}
         </>
     )
 }
