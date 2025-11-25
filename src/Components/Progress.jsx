a419b25ceaba9ac7d599947bfc1da3398765af03
Jamie Bryson <jamieab98@gmail.com>
2025-11-25 13:44:29 -0500

    Made it so the Progress page fetches data from the workout API and stores the data inside of workoutData

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,5 +1,19 @@
 import NavigationBar from "./NavigationBar"
+import { useEffect, useState } from "react"
+
 function Progress(){
+    const workoutsAPI = `https://6924d26482b59600d7217be2.mockapi.io/jamieab98workouts`
+    const [searchedExercise, setSearchedExercise] = useState("")
+    const [workoutData, setWorkoutData] = useState([])
+    
+    useEffect(()=>{
+        fetch(workoutsAPI)
+        .then(response=>response.json())
+        .then((data)=>{
+            setWorkoutData(data)
+            console.log(data)
+        })
+    }, [])
 
     //ToDo: Make it so the user can search by a specific exercise and it'll display all of those workouts in chronological order
 
@@ -7,6 +21,10 @@ function Progress(){
         <>
             <NavigationBar/>
             <h2>Progress Page</h2>
+            <form>
+                <label htmlFor="exercise">Exercise</label>
+                <input type="text" placeholder="exercise" value={searchedExercise} id="exercise" onChange={(e)=>setSearchedExercise(e.target.value)}/>
+            </form>
         </>
     )
 }
