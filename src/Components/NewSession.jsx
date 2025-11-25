23cd70baa3d0b8bd3a6978e2c70460a910f2edb0
Jamie Bryson <jamieab98@gmail.com>
2025-11-24 21:41:18 -0500

    Made it so submitting the form saves the userinfo as one exercise. This object gets saved in an array that will eventually get posted with fetch

--- src/Components/NewSession.jsx
+++ src/Components/NewSession.jsx
@@ -1,21 +1,40 @@
 import NavigationBar from "./NavigationBar"
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+
 function NewSession(){
+    const workoutsAPI = `https://6924d26482b59600d7217be2.mockapi.io/jamieab98workouts`
     const [exerciseName, setExerciseName] = useState("")
     const [exerciseWeight, setExerciseWeight] = useState(0)
     const [exerciseSets, setExerciseSets] = useState(0)
     const [exerciseReps, setExerciseReps] = useState(0)
+    const [exerciseDate, setExerciseDate] = useState("")
+    const [workout, setWorkout] = useState([])
 
-    function handleSubmit(e){
+    function enterExercise(e){
         e.preventDefault()
-        
+        const exercise = {
+            "exercise": exerciseName,
+            "weight": exerciseWeight,
+            "sets": exerciseSets,
+            "reps": exerciseReps
+        }
+        setWorkout([...workout, exercise])
+        setExerciseName("")
+        setExerciseWeight(0)
+        setExerciseSets(0)
+        setExerciseReps(0)
+    }
+
+    function enterWorkout(){
+        console.log(workout)
     }
 
     return(
         <>
             <NavigationBar/>
             <h2>Nice Workout! What did you do?</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={enterExercise}>
                 <label htmlFor="exercise">Exercise:</label>
                 <input type="text" placeholder="exercise" id="exercise" value={exerciseName} onChange={(e)=>setExerciseName(e.target.value)}></input>
                 <label htmlFor="weight">Weight:</label>
@@ -24,8 +43,11 @@ function NewSession(){
                 <input type="number" placeholder="sets" id="sets" value={exerciseSets} onChange={(e)=>setExerciseSets(e.target.value)}></input>
                 <label htmlFor="reps">Reps:</label>
                 <input type="number" placeholder="reps" id="reps" value={exerciseReps} onChange={(e)=>setExerciseReps(e.target.value)}></input>
-                <button type="submit">Submit Workout</button>
+                <label htmlFor="date">Date:</label>
+                <input type="date" id="date" value={exerciseDate} onChange={(e)=>setExerciseDate(e.target.value)}></input>
+                <button type="submit">Submit Exercise</button>
             </form>
+            <button type="button" onClick={enterWorkout}>Workout Complete</button>
         </>
     )
 }
