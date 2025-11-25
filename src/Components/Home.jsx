5cc8d9e3c830f44fdc793bd04c0b2e9c40af905a
Jamie Bryson <jamieab98@gmail.com>
2025-11-25 17:18:14 -0500

    Made it so the data on the home page is sorted by date instead of by the id

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import NavigationBar from "./NavigationBar"
 import { UsernameContext } from "../UsernameContext"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect} from "react"
 import DailyWorkout from "./DailyWorkout"
 
 function Home(){
@@ -14,15 +14,15 @@ function Home(){
             setWorkoutData(data)
         })
     }, [])
-    
-    //ToDo: Make it so the workouts load in order of date and not by id number//
+
+    const sortedWorkoutData = [...workoutData].sort((b,a)=>new Date(a.date) - new Date(b.date))    
 
     return(
         <>
             <NavigationBar/>
             <h2>Home Page</h2>
             <p>Welcome!</p>
-            {workoutData.map((day)=>(
+            {sortedWorkoutData.map((day)=>(
                 <DailyWorkout day={day} key={day.id}/>
             ))}
         </>
